5fd16a72d182fba3e1bbec4cfab57deb4f7cefa1
Alan Fay <emptyset@gmail.com>
2012-02-05 11:08:47 -0500

    some minor directory clean up.  for reference, look to the past

--- checkdaemon.sh
+++ checkdaemon.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-/usr/bin/ps awx | /usr/bin/grep DoorLoggerd.py | /usr/bin/grep -v grep >/dev/null 2>&1
+/usr/bin/ps awx | /usr/bin/grep vuvuzelad | /usr/bin/grep -v grep >/dev/null 2>&1
 if [ ! $? -eq 0 ]; then
-   logger "DoorLoggerd not running.  Restarting."
-   /home/DoorLogger/DoorLoggerd.py
+   logger "vuvuzelad not running.  Restarting."
+   /home/vuvuzela/bin/vuvuzelad.py
 fi
