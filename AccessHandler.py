a1b69860e52d0d4aae3bb31c1c9a4f9cae5767cf
Alan Fay <emptyset@gmail.com>
2012-02-11 17:33:13 -0500

    sketched out all the remaining code in the access handler, started the vuvuzela config file

--- AccessHandler.py
+++ AccessHandler.py
@@ -19,10 +19,6 @@ from twitter import Twitter, TwitterError
 from twitter import OAuth, read_token_file
 import ConfigParser
 
-# TODO: move the key and secret to a configuration file in /etc/vuvuzela
-CONSUMER_KEY='uS6hO2sV6tDKIOeVjhnFnQ'
-CONSUMER_SECRET='MEYTOS97VvlHX7K1rwHPEqVpTSqZ71HtvoK4sVuYk'
-
 def usage():
     print "Usage: "
     print "   %s ABCCCDDDDD" % sys.argv[0]
@@ -39,10 +35,70 @@ def run(program, *args):
         os.execvp(program, (program,) +  args)
 #    return os.wait()[0]
 
-def play_greeting(soundfile):
+def get_granted_sound(username):
+    # TODO: implement
+    #
+    # path = "vuvuzela:default_granted_sound" from /var/run/vuvuzela/config.ini
+    # detect ~<username>/.vuvuzela/config
+    #
+    # if exists
+    #     path = "greeting" field value in config file
+    #     
+    # return path
+    return ""
 
+def get_denied_sound():
+    # TODO: implement
+    #
+    # path = "vuvuzela:default_denied_sound" from /var/run/vuvuzela/config.ini
+    # return path
+    return ""
+
+def play_sound(path):
+    # TODO: test
+    run("/usr/bin/play", "-q", path, "trim", "0", "12")     
+    return
+
+def get_message(username):
+    # TODO: implement
+    #
+    # message = default message // "<username> has entered the building"
+    # detect ~<username>/.vuvuzela/config
+    #
+    # if exists
+    #     alias = "alias" field value in config file
+    #     message = "message" field value in config file
+    #
+    #     if alias exists
+    #         // modify the message, which presumably contains one %s
+    #         message = "message", alias
+    #
+    # return message
+    return ""
+
+def tweet_message(message):
+    # TODO: move the key and secret to a configuration file /var/run/vuvuzela/config.ini
+    CONSUMER_KEY='uS6hO2sV6tDKIOeVjhnFnQ'
+    CONSUMER_SECRET='MEYTOS97VvlHX7K1rwHPEqVpTSqZ71HtvoK4sVuYk'
+
+    # TODO: make sure we have a copy of the twitter.oauth file, or re-fetch from the twitter account
+    oauth_token, oauth_token_secret = read_token_file('/usr/local/etc/twitter.oauth')
+    tapi = Twitter( auth=OAuth( oauth_token, oauth_token_secret, CONSUMER_KEY, CONSUMER_SECRET), secure=1, api_version='1', domain='api.twitter.com' )
+
+    # Sometimes Twitter needs more than one attempt.
+    TwitRetry = 0   
+    while (TwitRetry < 4):
+        try:
+            tapi.statuses.update(status=message)
+            TwitRetry = 4
+        except:
+	        print 'Error updating twitter. Retrying...'
+
+        TwitRetry = TwitRetry + 1
+        time.sleep(5)
 
 def granted_sound(cardID):
+    # TODO: remove this function, using ~<username>/.vuvuzela/conig instead
     SOUNDPATH="/home/DoorSounds/"
     s_exists = 0
 
@@ -71,7 +127,7 @@ def granted_sound(cardID):
         run("/usr/bin/play", "-q", defaultsound)  # Play the generic welcome sound
 
 def dbconnect():
-    # TODO: remove this function; no longer going to use mysql login/password, only sqlite
+    # TODO: remove this method; no longer going to use mysql login/password, only sqlite
     dbhost="localhost"
     dbuser="logger"
     dbpw="SkyMotel"
@@ -88,6 +144,7 @@ def dbconnect():
     return conn
 
 def tweet_alias(cardID):
+    # TODO: remove this method
     # TODO: we can obtain the desired user alias from the previous sqlite query, passed in here
     conn = dbconnect()
     cursor = conn.cursor()
@@ -103,37 +160,52 @@ def tweet_alias(cardID):
 
     cursor.close()
 
-    # TODO: make sure we have a copy of the twitter.oauth file, or re-fetch from the twitter account
-    oauth_token, oauth_token_secret = read_token_file('/usr/local/etc/twitter.oauth')
-    tapi = Twitter( auth=OAuth( oauth_token, oauth_token_secret, CONSUMER_KEY, CONSUMER_SECRET), secure=1, api_version='1', domain='api.twitter.com' )
-    # TODO: use a substitution string found in the user's .vuvuzela/config, or a default defined here
-    update = "Granted: %s" % alias
+def has_access(flag):
+    # TODO: test
+    return (flag == "V") | (flag == "F")
 
-    # Sometimes Twitter needs more than one attempt.
-    TwitRetry = 0   
-    while (TwitRetry < 4):
-        try:
-            tapi.statuses.update(status=update)
-            TwitRetry = 4
-        except:
-	        print 'Error updating twitter. Retrying...'
+def get_username(cardID):
+    # TODO: implement
+    #
+    # make a connection to the /var/run/vuvuzela/vuvuzela.db sqlite file
+    # select from the members table the username for the cardID
+    # return username
+    return ""
 
-        TwitRetry = TwitRetry + 1
-        time.sleep(5)
+def execute_granted_actions(username):
+    # TODO: test 
+    soundpath = get_granted_sound(username)
+    message = get_message(username)
+
+    play_sound(soundpath)
+    tweet_message(message)
+
+def execute_denied_actions():
+    # TODO: test
+    soundpath = get_denied_sound()
+
+    play_sound(soundpath)
 
 def main():
-    argc=len(sys.argv)
+    argc = len(sys.argv)
     if (argc != 2):
         usage()
 
     # We want our children to die quietly
     signal.signal(signal.SIGCHLD, signal.SIG_IGN)  
 
-    line=re.sub('[^A-Z0-9]+','', sys.argv[1])[:10]
-    A_result = line[1]
-    A_cardID = line[2:]
+    line = re.sub('[^A-Z0-9]+','', sys.argv[1])[:10]
+    flag = line[1]
+    cardID = line[2:]
 
-    if ( (A_result == "V") | (A_result == "F") ):
+    if ( has_access(flag) )
+        username = get_username(cardID)
+        execute_granted_actions(username)
+    else:
+        execute_denied_actions() 
+
+    # TODO: remove this section
+    if ( (flag == "V") | (cardID == "F") ):
         # Access Granted
         
         # Play sound if it exists
