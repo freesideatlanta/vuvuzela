f8f576239f37a0f4fe62d05ac1c9ab155fe31ce5
Alan Fay <emptyset@gmail.com>
2012-02-12 09:34:59 -0500

    forgot to include the location id in the entrylog table

--- data/create_entrylog.sql
+++ data/create_entrylog.sql
@@ -3,6 +3,7 @@ begin transaction;
 create table entrylog(
 	id INTEGER NOT NULL,
 	card_id VARCHAR(8) NOT NULL,
+	location_id VARCHAR(3) NOT NULL,
 	strike_flag CHARACTER(1) NOT NULL,
 	strike_time DATETIME NOT NULL);
 
